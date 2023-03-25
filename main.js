94b5b0fd068b471babd5268603c09641c034ec16
Cesar Henrique Martins da Rosa <chmr.developer@gmail.com>
2023-03-25 02:37:14 -0300

    Tray implemented
    - icons added
    - quit functionality
    - x button hides window instead quiting
    - funny quiting phrases

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
+const { app, BrowserWindow, Tray, nativeImage, Menu, ipcMain } = require('electron');
 const path = require('path')
 
+let mainWindow;
+let tray;
+const icon = nativeImage.createFromPath(path.resolve(__dirname, 'src', 'images', 'icons', 'icon_64.png'))
+
 const createWindow = () => {
-    const win = new BrowserWindow({
+    const window = new BrowserWindow({
         width: 300,
         height: 400,
         titleBarStyle: 'hidden',
@@ -17,14 +21,30 @@ const createWindow = () => {
         }
     })
     
-    ipcMain.handle('close', () => win.close())
-    ipcMain.handle('minimize', () => win.minimize())
+    ipcMain.handle('close', () => window.hide())
+    ipcMain.handle('minimize', () => window.minimize())
     
-    win.loadFile(path.join(__dirname, 'src', 'index.html'))
+    window.loadFile(path.join(__dirname, 'src', 'index.html'))
+
+    return window
+}
+
+function createTray(icon){
+    const tray = new Tray(icon)
+    const contextMenu = Menu.buildFromTemplate([
+        { label: quitPhrases[getRandomNumber(0,quitPhrases.length)] , click: () => app.quit() },
+    ])
+    tray.setContextMenu(contextMenu)
+    tray.setToolTip('Open Donut Sweeper')
+    tray.setTitle('Title')
+    tray.on('click', () => mainWindow.show())
+    return tray
 }
 
 app.whenReady().then(() => {
-    createWindow()
+    mainWindow = createWindow()
+    tray = createTray(icon)
+
 
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
@@ -34,3 +54,19 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
     if(process.plataform !== 'darwin') app.quit()
 })
+
+app.on('before-quit', () => {
+    tray.destroy()
+})
+
+const quitPhrases = [
+    'Donut leave!!!',
+    'Donut shut it down, please!',
+    'I Donut believe that you leaving!',
+    'Dunot do this!',
+    'Dunot click this button!'
+]
+
+function getRandomNumber(min, max){
+    return Math.round((Math.random() * max) + min)
+}
